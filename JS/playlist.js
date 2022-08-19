672bd13dd8c42a5e97dd037dc953bc5f9a709ab8
Boris Kaminskiy <93283934+BorisKaminskiy@users.noreply.github.com>
2022-08-19 13:17:01 +0300

    Update playlist.js

--- JS/playlist.js
+++ JS/playlist.js
@@ -25,60 +25,6 @@ const playList =
     "duration": "06:00"
   },
   {
-    "title": "symphony_of_the_forest",
-    "author": "KITARO",
-    "src": "../assets/sounds/KITARO_-_symphony_of_the_forest_37389934.mp3",
-    "duration": "04:44"
-  },
-  {
-    "title": "Someone_Somewhere",
-    "author": "Redlounge_Orchestra",
-    "src": "../assets/sounds/Redlounge_Orchestra_-_Someone_Somewhere_37389696.mp3",
-    "duration": "04:42"
-  },
-  {
-    "title": "The_Ocean_Goldtripp_remix",
-    "author": "Oxygene",
-    "src": "../assets/sounds/Oxygene_-_The_Ocean_Goldtripp_remix_37389619.mp3",
-    "duration": "05:32"
-  },
-  {
-    "title": "Walking_on_the_Clouds",
-    "author": "Nikonn",
-    "src": "../assets/sounds/Nikonn_-_Walking_on_the_Clouds_37389792.mp3",
-    "duration": "04:05"
-  },
-  {
-    "title": "Be_As_One_Klangstein_Remix",
-    "author": "Minus_Blue_feat_Emma_Saville",
-    "src": "../assets/sounds/Minus_Blue_feat_Emma_Saville_-_Be_As_One_Klangstein_Remix_37389777.mp3",
-    "duration": "05:50"
-  },
-  {
-    "title": "Believe_Me",
-    "author": "Manu_Shrine",
-    "src": "../assets/sounds/Manu_Shrine_-_Believe_Me_37389718.mp3",
-    "duration": "03:42"
-  },
-  {
-    "title": "Old_Home_Movie",
-    "author": "Goran_Bregovic",
-    "src": "../assets/sounds/Goran_Bregovic_-_Old_Home_Movie_37389712.mp3",
-    "duration": "05:01"
-  },
-  {
-    "title": "Sweet_Word_Martian_Chillout_Remix",
-    "author": "Freeman_and_Drilla",
-    "src": "../assets/sounds/Freeman_and_Drilla_-_Sweet_Word_Martian_Chillout_Remix_37389610.mp3",
-    "duration": "04:02"
-  },
-  {
-    "title": "Shepherd_Moons_2009_Remastered_Version",
-    "author": "Enya_-_Shepherd",
-    "src": "../assets/sounds/Enya_-_Shepherd_Moons_2009_Remastered_Version_37389788.mp3",
-    "duration": "03:45"
-  },
-  {
     "title": "My_Lovers_Gone",
     "author": "Dido",
     "src": "../assets/sounds/Dido_-_My_Lovers_Gone_28545560.mp3",
